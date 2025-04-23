66ed4a1533ea4b2b704731f051c0057b3fd8bdb3
AntoineRoss0516 <antoine.ross.16@outlook.com>
2025-04-23 17:38:48 -0400

    aVANCEMENT MAJEUR DU TRAVAIL

--- Assets/Scripts/MiniJeu.cs
+++ Assets/Scripts/MiniJeu.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MiniJeu : MonoBehaviour
 {
     public float pointageTemps;
     public TextMeshProUGUI textScore;
-
+    public TextMeshProUGUI textNouveauRecord;
+    public GameObject finPanel;
+    public TMP_InputField inputNom;
     void Start()
     {
         pointageTemps = 0;
@@ -16,5 +19,23 @@ public class MiniJeu : MonoBehaviour
     private void Update()
     {
         textScore.text = pointageTemps.ToString("00.00");
+    }
+
+    public void OnPanel()
+    {
+        float recordActuel = PlayerPrefs.GetFloat("meilleurScore", 0f);
+        if (pointageTemps > recordActuel)
+        {
+          finPanel.SetActive(true);
+          textNouveauRecord.text = textScore.text;  
+        }
+         
+    }
+
+    public void ConfirmerNouveauRecord()
+    {
+        finPanel.SetActive(false);
+        string nom = inputNom.text;
+        PlayerPrefs.SetString("nomJoueur", nom);
     }
 }
